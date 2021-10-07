f09249b3d8c43497b31500491b7fd64768d9dfcd
doublewill <xd8012@163.com>
2021-10-07 16:43:14 +0800

    re contruct project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import logo from './logo.svg';
-import './styles/base.scss';
+import 'antd/dist/antd.css';
+import './styles/index.scss';
+import { Layout, Menu } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import Dashboard from './views/dashboard/index';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="app-main">
+      <Header className="header">
+        <div className="logo" />
+      </Header>
+      <Layout className="app-wrapper">
+        <Sider width={200} className="side-layout">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <SubMenu key="sub1" icon={<UserOutlined />} title="主页">
+              <Menu.Item key="1">仪表板</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<LaptopOutlined />} title="图表">
+              <Menu.Item key="5">Echarts</Menu.Item>
+              <Menu.Item key="6">Charts</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" icon={<NotificationOutlined />} title="表格">
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Content className="app-content">
+          <Dashboard item="hello"></Dashboard>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
